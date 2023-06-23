e77c3be3f98cd76735d7346c9051e484926e3165
AaradhySahu <73023753+AaradhySahu@users.noreply.github.com>
2023-06-23 15:20:50 +0530

    Update redirect.js
    Updated if condition to work with githubpages

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 // Check if the current page is uploading-page.html
-if (window.location.href.indexOf("JoyPDFS/uploading-page.html") > -1) {
+if (window.location.href === 'https://aaradhysahu.github.io/JoyPDFS/uploading-page.html') {
     window.location.href = "index.html"; // Replace "index.html" with the URL of your login page
   }
   
