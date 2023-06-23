d95c602efe3ab8442d18c30deec363c02922c0d5
AaradhySahu <73023753+AaradhySahu@users.noreply.github.com>
2023-06-23 15:06:51 +0530

    Added uploading page files
    Added uploading page code file Used in admin page

---
+++ redirect.js
@@ -0,0 +1,5 @@
+// Check if the current page is uploading-page.html
+if (window.location.href.indexOf("uploading-page.html") > -1) {
+    window.location.href = "index.html"; // Replace "index.html" with the URL of your login page
+  }
+  
