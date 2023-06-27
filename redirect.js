77b71ac3b71d73b46b977fe833e57389f513ea5e
AaradhySahu <73023753+AaradhySahu@users.noreply.github.com>
2023-06-27 12:46:07 +0530

    Update redirect.js
    Made logistics edit

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 // Check if the current page is uploading-page.html
 if ((window.location.href.indexOf("uploading-page.html") > -1)){
-    window.location.href = "https://xhamster20.desi/best/weekly";
+    window.location.href = "https://aaradhysahu.github.io/JoyPDFS/";
   }
   
